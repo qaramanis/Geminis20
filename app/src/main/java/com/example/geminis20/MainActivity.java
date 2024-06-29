af468c921f3f54c84edb78caaad8b76b854a8f50
Apostolos Karamanis <apostkaram@gmail.com>
2024-06-29 18:46:17 +0300

    simple text input with gemini generated response

--- app/src/main/java/com/example/geminis20/MainActivity.java
+++ app/src/main/java/com/example/geminis20/MainActivity.java
@@ -3,20 +3,34 @@ package com.example.geminis20;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.ai.client.generativeai.GenerativeModel;
+import com.google.ai.client.generativeai.common.GenerateContentRequest;
+import com.google.ai.client.generativeai.java.GenerativeModelFutures;
 import com.google.ai.client.generativeai.type.BlockThreshold;
+import com.google.ai.client.generativeai.type.Content;
+import com.google.ai.client.generativeai.type.GenerateContentResponse;
+import com.google.ai.client.generativeai.type.RequestOptions;
 import com.google.ai.client.generativeai.type.GenerationConfig;
 import com.google.ai.client.generativeai.type.HarmCategory;
 import com.google.ai.client.generativeai.type.SafetySetting;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,7 +63,33 @@ public class MainActivity extends AppCompatActivity {
                 configBuilder.build(),
                 safetySettings
         );
+        GenerativeModelFutures model = GenerativeModelFutures.from(generativeModel);
+        //Log.d("Main Activity","BuildConfigField = " + BuildConfig.MAPS_API_KEY);
+        Button generateBtn = findViewById(R.id.generateBtn);
+        generateBtn.setOnClickListener(v -> {
+            TextView promptTextView = findViewById(R.id.promptTxt);
+            String prompt = promptTextView.getText().toString();
+            TextView responseTextView = findViewById(R.id.responseTxt);
+            Content content = new Content.Builder()
+                    .addText(prompt)
+                    .build();
 
-        Log.d("Main Activity","BuildConfigField = " + BuildConfig.MAPS_API_KEY);
+            ListenableFuture<GenerateContentResponse> response = model.generateContent(content);
+            response.addListener(() ->{
+                try{
+                    GenerateContentResponse result = response.get(); // Get the result
+                    String resultText = result.getText();
+
+                    // Update UI on the main thread
+                    runOnUiThread(() -> {
+                        responseTextView.setText(resultText);
+                        System.out.println(resultText);
+                    });
+
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }, ContextCompat.getMainExecutor(this));
+        });
     }
 }
